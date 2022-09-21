7820f895f4536f113a0711fd72daed14385895be
Katia <katiacristinedeoliveira@gmail.com>
2022-09-21 14:22:57 -0300

    First commit

--- src/br/com/dio/PrimeiroProograma.java
+++ src/br/com/dio/PrimeiroProograma.java
@@ -7,15 +7,9 @@ public class PrimeiroProograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livros livros = new Livros();
 		
 		System.out.println(gato);
-		System.out.println(livros);
-		
+	
 	}
 }
 	
-class Livros {
-	private String nome;
-	private String npag;
-}
